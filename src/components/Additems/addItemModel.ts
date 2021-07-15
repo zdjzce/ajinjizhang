9823b06bfc6ab4b54fe001fd23294c3e9dafce4f
zdj <493904954@qq.com>
2021-07-15 22:26:23 +0800

    按钮

---
+++ src/components/Additems/addItemModel.ts
@@ -0,0 +1,20 @@
+interface icon {
+  type?: string
+  name?: string
+  icon?: string
+  color?: string
+}
+const AddItemModel: icon[] = [
+  { name: '电影', icon: 'dianying', color: 'feiji' },
+  { name: '聚会', icon: 'juhui', color: 'jiushui' },
+  { name: 'K歌', icon: 'kge', color: 'kanbing' },
+  { name: '棋牌', icon: 'qipai', color: 'huafei' },
+  { name: '射箭', icon: 'shejian', color: 'gouwu' },
+  { name: '游戏', icon: 'youxi', color: 'jiaotong' },
+  { name: '肉', icon: 'rou', color: 'jianshen' },
+  { name: '外卖', icon: 'waimai', color: 'jiushui' },
+  { name: '牛奶', icon: 'niunai', color: 'kanbing' },
+  { name: '小吃', icon: 'xiaochi', color: 'jianshen' },
+  { name: '主食', icon: 'zhushi', color: 'jiushui' },
+  { name: '零食', icon: 'lingshi', color: 'gouwu' }
+]
