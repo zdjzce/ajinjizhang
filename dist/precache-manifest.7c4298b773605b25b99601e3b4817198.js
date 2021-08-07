c5ab17f7114c580e11d2f736408831081e258810
zdj <493904954@qq.com>
2021-08-07 13:40:28 +0800

    增加dist

---
+++ dist/precache-manifest.7c4298b773605b25b99601e3b4817198.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9a6326699e293d23e21",
+    "url": "/ajinjizhang---website/css/app.8a16dfed.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/ajinjizhang---website/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/ajinjizhang---website/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "b5421f46ad1ac119e2ea1bce24e0e9a0",
+    "url": "/ajinjizhang---website/img/bgc.b5421f46.png"
+  },
+  {
+    "revision": "be9c634b1ed6dd714cd2f186418ae961",
+    "url": "/ajinjizhang---website/index.html"
+  },
+  {
+    "revision": "b9a6326699e293d23e21",
+    "url": "/ajinjizhang---website/js/app.01dfa4e2.js"
+  },
+  {
+    "revision": "468faba0ea20307a4706",
+    "url": "/ajinjizhang---website/js/chunk-vendors.3191aaa8.js"
+  },
+  {
+    "revision": "498755eee41ad57ef154cdc65fdf7641",
+    "url": "/ajinjizhang---website/manifest.json"
+  },
+  {
+    "revision": "d46d27f3c15a8a1b8c219748d8162878",
+    "url": "/ajinjizhang---website/normalize.css"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/ajinjizhang---website/robots.txt"
+  }
+]);
